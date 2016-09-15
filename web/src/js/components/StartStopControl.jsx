3ad0220255dbd00ff498d1aa562ae10b83fbe650
Felix Ebert <felix.ebert@if-core.de>
2016-09-15 23:55:57 +0200

    start stop control

---
+++ web/src/js/components/StartStopControl.jsx
@@ -0,0 +1,42 @@
+import React, {Component} from "react";
+import axios from "axios";
+import RaisedButton from "material-ui/RaisedButton";
+import {showNotification} from "../actions/notificationActions";
+import {connect} from "react-redux";
+import {Card, CardTitle, CardText} from "material-ui/Card";
+
+class StartStopControl extends Component {
+    sendState(state) {
+        const {dispatch} = this.props;
+        axios.post(`/api/devices/masterOverride`, {
+            state: state ? 1 : 0
+        })
+            .then(() => {
+                dispatch(showNotification(`Alle Räume sind nun ${state ? 'an' : 'aus'}`));
+            })
+            .catch((error) => {
+                console.trace(error.stack);
+                dispatch(showNotification(error.message));
+            });
+    }
+
+    render() {
+        return (
+            <section style={{marginTop: '1rem'}}>
+                <Card>
+                    <CardTitle title="Master Control" subtitle="Alle Räume ..."/>
+                    <CardText>
+                        <RaisedButton label="An" primary={true} style={{marginRight: '1rem'}}
+                                      onClick={this.sendState.bind(this, true)}/>
+                        <RaisedButton label="Aus" primary={true}
+                                      onClick={this.sendState.bind(this, false)}/>
+                    </CardText>
+                </Card>
+            </section>
+        );
+    }
+}
+
+const mapStateToProps = (state) => ({});
+
+export default connect(mapStateToProps)(StartStopControl);
