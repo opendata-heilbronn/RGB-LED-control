b95274aabcb7417254446999c015b42ee735dd9e
Adrian Stabiszewski <as@grundid.de>
2016-12-22 15:14:49 +0100

    small fix

--- server/RGB_anim.js
+++ server/RGB_anim.js
@@ -62,29 +62,28 @@ function startAnim(name) {
     //}, 0);
 }
 
-function changeRoom(roomNumber){
+function changeRoom(roomNumber) {
     var mac = rgbControls.roomToMAC(roomNumber);
-    
-    if(obj.fade == 0) {
-      rgbControls.sendRGB(mac, obj.color);
+
+    if (obj.fade == 0) {
+        rgbControls.sendRGB(mac, obj.color);
     }
     else {
-      rgbControls.sendFade(mac, obj.color, obj.fade);
+        rgbControls.sendFade(mac, obj.color, obj.fade);
     }
-}    
+}
 
-function doFrame(frame)
-{
-  var frameData = frame.frameData;
-  frameData.forEach(function(obj){ //TODO all rooms
-    if($.isArray(obj.room)){
-        obj.room.forEach(function(room){
-            changeRoom(room);
-        });
-    }else{
-        changeRoom(obj.room);
-    }
-  });
+function doFrame(frame) {
+    var frameData = frame.frameData;
+    frameData.forEach(function (obj) { //TODO all rooms
+        if (Array.isArray(obj.room)) {
+            obj.room.forEach(function (room) {
+                changeRoom(room);
+            });
+        } else {
+            changeRoom(obj.room);
+        }
+    });
 
     ++animIdx;
     if (animIdx >= animLength)
