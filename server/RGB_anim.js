3f814dd04c118da2836ea6793c2342cfb9e9a33e
Adrian Stabiszewski <as@grundid.de>
2016-12-22 15:12:03 +0100

    added coworotate anim, save new animations in user animations

--- server/RGB_anim.js
+++ server/RGB_anim.js
@@ -1,96 +1,92 @@
 const rgbControls = require('./RGB_control');
 var fs = require('fs');
-var animFileName = "anims.json";
+var defaultAnimations = "anims.json";
+var userAnimations = "anims-user.json";
 
-var animations = {}
+var animations = {};
 
-function writeAnimFile()
-{
-  var json = JSON.stringify(animations);
-  fs.writeFile(animFileName, json, 'utf8', function(){console.log("Saving animation file done.")});
+function writeAnimFile() {
+    var json = JSON.stringify(animations);
+    fs.writeFile(userAnimations, json, 'utf8', function () {
+        console.log("Saving animation file done.")
+    });
 }
 
-function readAnimFile()
-{
-  fs.readFile(animFileName, (err, data) => {
-    if (err) throw err;
-    animations = JSON.parse(data);
-    //console.log(getAnimNames());
-    //console.log(getAnim("anim2"));
-  });
-}
-readAnimFile();
-
-function getAnimNames()
-{
-  var keys = Object.keys(animations);
-  var ret = {};
-  ret.animations = keys;
-  return ret;
+function readAnimFile() {
+    var content = fs.readFileSync(defaultAnimations);
+    animations = JSON.parse(content);
+    if (fs.existsSync(userAnimations)) {
+        content = fs.readFileSync(userAnimations);
+        var userAnimations = JSON.parse(content);
+        Object.assign(animations, userAnimations);
+    }
 }
 
-function getAnim(name)
-{
-  var ret = {};
-  ret[name] = animations[name];
-  return ret;
+function getAnimNames() {
+    var keys = Object.keys(animations);
+    var ret = {};
+    ret.animations = keys;
+    return ret;
 }
 
-function saveAnim(name, data)
-{
-  animations[name] = {};
-  animations[name] = data;
-  writeAnimFile();
-  if(curAnim == name)
-    startAnim(name);
+function getAnim(name) {
+    var ret = {};
+    ret[name] = animations[name];
+    return ret;
 }
 
-function deleteAnim(name)
-{
-  delete animations[name];
-  writeAnimFile();
+function saveAnim(name, data) {
+    animations[name] = {};
+    animations[name] = data;
+    writeAnimFile();
+    if (curAnim == name)
+        startAnim(name);
+}
+
+function deleteAnim(name) {
+    delete animations[name];
+    writeAnimFile();
 }
 
 var animIdx = 0;
 var animLength;
 var curAnim = "";
 var animTimeout;
-function startAnim(name)
-{
-  stopAnim();
-  curAnim = name;
-  animLength = animations[name].length
-  //animTimeout = setTimeout(function(){
+
+function startAnim(name) {
+    stopAnim();
+    curAnim = name;
+    animLength = animations[name].length;
+    //animTimeout = setTimeout(function(){
     doFrame(animations[name][0]);
-  //}, 0);
+    //}, 0);
 }
 
-function doFrame(frame)
-{
-  var frameData = frame.frameData;
-  frameData.forEach(function(obj){ //TODO all rooms
-    var mac = rgbControls.roomToMAC(obj.room);
-    if(obj.fade == 0) {
-      rgbControls.sendRGB(mac, obj.color);
-    }
-    else {
-      rgbControls.sendFade(mac, obj.color, obj.fade);
-    }
-  });
+function doFrame(frame) {
+    var frameData = frame.frameData;
+    frameData.forEach(function (obj) { //TODO all rooms
+        var mac = rgbControls.roomToMAC(obj.room);
+        if (obj.fade == 0) {
+            rgbControls.sendRGB(mac, obj.color);
+        }
+        else {
+            rgbControls.sendFade(mac, obj.color, obj.fade);
+        }
+    });
 
-  ++animIdx;
-  if(animIdx >= animLength)
+    ++animIdx;
+    if (animIdx >= animLength)
+        animIdx = 0;
+    animTimeout = setTimeout(function () {
+        doFrame(animations[curAnim][animIdx])
+    }, frame.pause);
+}
+
+function stopAnim() {
     animIdx = 0;
-  animTimeout = setTimeout(function(){
-    doFrame(animations[curAnim][animIdx])
-  }, frame.pause);
+    clearTimeout(animTimeout);
 }
 
-function stopAnim()
-{
-  animIdx = 0;
-  //curAnim = "";
-  clearTimeout(animTimeout);
-}
+readAnimFile();
 
 module.exports = {getAnimNames, getAnim, saveAnim, deleteAnim, startAnim, stopAnim}
