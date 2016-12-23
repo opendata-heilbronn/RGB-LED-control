e6b3ed5e6c86d22d478f0982dc0848b3d5972ac3
Simon <simon@Arkon.fritz.box>
2016-12-23 14:29:06 +0100

    Added stopAnim in some cases

--- server/RGB_anim.js
+++ server/RGB_anim.js
@@ -54,7 +54,6 @@ var curAnim = "";
 var animTimeout;
 
 function startAnim(name) {
-    console.log(name);
     stopAnim();
     curAnim = name;
     animLength = animations[name].length;
@@ -104,12 +103,15 @@ function stopAnim() {
 
 function setAnimation(name){
     if(name == 'off'){
+            stopAnim();
             rgbControls.fadeOff(0);
     }else if(name == 'party'){
+            stopAnim();
             Object.keys(rgbControls.devices).forEach(function(key){
                 rgbControls.startParty(key); //start party mode
             });
     }else if(name == 'lighthouse'){
+            stopAnim();
             rgbControls.startLighthouse();
     }else{
             startAnim(name);
