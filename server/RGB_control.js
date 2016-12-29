13adffd715277e6951e715d0936549315e5a6670
Adrian Stabiszewski <as@grundid.de>
2016-12-29 22:09:59 +0100

    fix

--- server/RGB_control.js
+++ server/RGB_control.js
@@ -204,7 +204,7 @@ client.on('message', function (topic, message) {
             console.log('unknown device ' + mac);
             return false;
         } else {
-            if (message) {
+            if (message && message != "") {
                 var currentDevice = devices[mac];
                 var subArgs = message.indexOf("&") != -1  ? message.split("&") : [message];
                 subArgs.forEach(function(element) {
